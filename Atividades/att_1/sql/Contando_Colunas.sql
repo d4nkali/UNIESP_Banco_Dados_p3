b355e65f82b3c8c13b02c6fcf0e9c41a16430413
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-08-23 21:03:09 -0300

    Script sql de contagem das chaves primerias

---
+++ Atividades/att_1/sql/Contando_Colunas.sql
@@ -0,0 +1,5 @@
+-- Contando as colunas das Chaves Primarias das tabelas.
+
+select count(cod_produto) from public.produtos;
+select count(cod_usuario) from public.usuarios;
+select count(cod_produto) from public.vendas;
