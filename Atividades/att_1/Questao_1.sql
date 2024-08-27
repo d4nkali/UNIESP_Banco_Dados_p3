3e9b2ac53c497a805737ef24e2f625f9197e24fa
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-08-27 16:05:35 -0300

    Questao 1

--- Atividades/att_1/Questao_1.sql
+++ Atividades/att_1/Questao_1.sql
@@ -1,5 +1,12 @@
 /*
 
+Questão 1 - Crie uma query que apresente os usuários e o produto comprado.
 
+Aluno: Danilo Pereira - P3B
 
 */
+
+select v.cod_usuario, p.nome_produto
+from public.produtos p 
+join vendas v 
+on p.cod_produto = v.cod_produto;
