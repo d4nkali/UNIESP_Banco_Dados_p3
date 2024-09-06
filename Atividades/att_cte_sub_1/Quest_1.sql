31e3b8d52b4bfa472afd87f5829938feb5c1565e
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-09-06 20:12:26 -0300

    Corrigindo questao 1

--- Atividades/att_cte_sub_1/Quest_1.sql
+++ Atividades/att_cte_sub_1/Quest_1.sql
@@ -1,5 +1,14 @@
--- Questão 1 - Crie uma query do tipo subquery que apresente os produtos vendidos por usuário
-select v.cod_usuario, p.nome_produto
-from public.produtos p 
-join vendas v 
-on p.cod_produto = v.cod_produto;
+-- Questão 1 - Crie uma query do tipo subquery que apresente os produtos vendidos por usuário.
+WITH compras_2023_cte AS (
+    SELECT v.cod_usuario, p.nome_produto, v.data_compra
+    FROM public.produtos p
+    JOIN vendas v
+    ON p.cod_produto = v.cod_produto
+    WHERE data_compra = (
+        SELECT MAX(v2.data_compra)
+        FROM vendas v2 
+        WHERE EXTRACT(YEAR FROM v2.data_compra::date) < 2023
+    )
+    order by v.cod_usuario
+)
+SELECT * FROM compras_2023_cte;
