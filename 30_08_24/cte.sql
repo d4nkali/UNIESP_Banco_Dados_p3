f00c26e4fdaac2f692b2e46c298d6550c0c56345
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-09-06 19:34:52 -0300

    Corrigindo cte.sql

--- 30_08_24/cte.sql
+++ 30_08_24/cte.sql
@@ -1,11 +1,11 @@
 -- Script SQL para realizar uma CTE.
 
 WITH compras_2023_cte AS (
-    SELECT v.cod_usuario, p.nome_produto
+    SELECT v.cod_usuario, p.nome_produto, v.data_compra
     FROM public.produtos p
     JOIN vendas v
     ON p.cod_produto = v.cod_produto
-    WHERE v.data_compra = (
+    WHERE data_compra = (
         SELECT MAX(v2.data_compra)
         FROM vendas v2 
         WHERE EXTRACT(YEAR FROM v2.data_compra::date) < 2023
