f9ceb3b21eaf2d9322ec4794a310b07148a26de2
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-09-30 19:41:14 -0300

    Adicionando questao 1

---
+++ Atividades/att_cte_sub_2/Quest_1.sql
@@ -0,0 +1,10 @@
+-- Questão 1: Escreva uma subconsulta para encontrar os usuários que compraram produtos na categoria 'Eletrônicos'.
+with usuarios_compras_eletronicos as (
+	select v.cod_usuario, p.nome_produto, p.categoria_produto, v.data_compra
+	from public.produtos p 
+	join vendas v 
+	on p.cod_produto = v.cod_produto
+	where p.categoria_produto = 'Eletronicos'
+	order by v.cod_usuario
+)
+select * from usuarios_compras_eletronicos;
---
+++ Atividades/att_cte_sub_2/README.md
@@ -0,0 +1,5 @@
+# Exercícios - Subquerys || CTE_2
+
+1. Escreva uma subconsulta para encontrar os usuários que compraram produtos na categoria 'Eletrônicos'.
+
+2.
