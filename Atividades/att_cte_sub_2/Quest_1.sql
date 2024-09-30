b47f00ea723f0925af302f9fb9f7296daf1ec93a
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-09-30 19:49:57 -0300

    Adicionando questao 1

--- Atividades/att_cte_sub_2/Quest_1.sql
+++ Atividades/att_cte_sub_2/Quest_1.sql
@@ -1,4 +1,9 @@
--- Questão 1: Escreva uma subconsulta para encontrar os usuários que compraram produtos na categoria 'Eletrônicos'.
+/*
+
+Questão 1: Escreva uma subconsulta para encontrar os usuários que compraram 
+produtos na categoria 'Eletrônicos'.
+
+*/
 with usuarios_compras_eletronicos as (
 	select v.cod_usuario, p.nome_produto, p.categoria_produto, v.data_compra
 	from public.produtos p 
