12ecb9c79ed06d3a905e982e9be9c4d0556f1f1d
d4nkali <133923419+d4nkali@users.noreply.github.com>
2024-10-01 10:53:24 -0300

    Update

--- Atividades/att_cte_sub_2/Quest_2.sql
+++ Atividades/att_cte_sub_2/Quest_2.sql
@@ -4,23 +4,23 @@ Questão 2 - Utilizando uma CTE, calcule a média de valor de compra por usuári
 usuários cuja média de compra seja superior a 500.
 
 */
-WITH ComprasPorUsuario AS (
-    SELECT
+with ComprasPorUsuario as (
+    select
         u.cod_usuario,
-        COALESCE(SUM(v.valor_compra), 0) AS total_compras,
-        COUNT(v.cod_produto) AS num_compras
-    FROM usuarios u
-    LEFT JOIN vendas v ON u.cod_usuario = v.cod_usuario
-    GROUP BY u.cod_usuario
+        coalesce(sum(v.valor_compra), 0) as total_compras,
+        count(v.cod_produto) as num_compras
+    from usuarios u
+    left join vendas v on u.cod_usuario = v.cod_usuario
+    group by u.cod_usuario
 ),
-MediaValorCompra AS (
-    SELECT
+MediaValorCompra as (
+    select
         cod_usuario,
-        round(total_compras::numeric / NULLIF(num_compras, 0), 2) AS media_compra
-    FROM ComprasPorUsuario
+        round(total_compras::numeric / NULLIF(num_compras, 0), 2) as media_compra
+    from ComprasPorUsuario
 )
-SELECT 
+select 
     cod_usuario,
     media_compra
-FROM MediaValorCompra
-WHERE media_compra > 500;
+from MediaValorCompra
+where media_compra > 500;
